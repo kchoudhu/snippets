ae1355bc5b24a9cdbdeff3fd45dd4ae019345b9a
Kamil Choudhury <kamil.choudhury@anserinae.net>
2016-08-16 14:08:47 +0300

    Add ability to install on different Unices

--- st3/install.sh
+++ st3/install.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 
-cp *.sublime-snippet ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/
+CFGDIR=~/.config/sublime-text-3
+if [ `uname` = "Darwin" ]; then
+	CFGDIR=~/Library/Application\ Support/Sublime\ Text\ 3
+fi
+
+cp *.sublime-snippet ${CFGDIR}/Packages/User/
 
